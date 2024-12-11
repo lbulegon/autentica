c2e85e2da4c118af2508bc6006f7faa1636ba269
lbulegon <lbulegon@gmail.com>
2024-12-10 21:39:38 -0300

    outras rotas

--- autentica/urls.py
+++ autentica/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, re_path
 from api_v01.views  import LoginView, home_view
 from django.conf import settings
 from django.views.static import serve
+from motopro.views import home, UserListView, UserCreateView, UserUpdateView, UserDeleteView, VagaListView, VagaCreateView, VagaUpdateView, VagaDeleteView, EmpresaCreateView,EmpresaDeleteView,EmpresaDetailView,EmpresaListView,EmpresaUpdateView
 
 
 urlpatterns = [
@@ -27,6 +28,29 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('home/', home_view),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
+   
+    ######### users   ####################
+    path('users/',                 UserListView.as_view(),   name='user-list'),
+    path('users/create/',          UserCreateView.as_view(), name='user-create'),
+    path('users/update/<int:pk>/', UserUpdateView.as_view(), name='user-update'),
+    path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),
+   
+   ########## vagas ################
+    path('vagas/',                 VagaListView.as_view(),   name='vaga-list'),
+    path('vagas/create/',          VagaCreateView.as_view(), name='vaga-create'),
+    path('vagas/update/<int:pk>/', VagaUpdateView.as_view(), name='vaga-update'),
+    path('vagas/delete/<int:pk>/', VagaDeleteView.as_view(), name='vaga-delete'),
+
+    ########## Empesas ################
+    path('empresas/',                EmpresaListView.as_view(),   name='empresa-list'),  # Lista de empresas
+    path('empresa/<int:pk>/',        EmpresaDetailView.as_view(), name='empresa-detail'),  # Detalhe de uma empresa
+    path('empresa/create/',          EmpresaCreateView.as_view(), name='empresa-create'),  # Criação de empresa
+    path('empresa/<int:pk>/update/', EmpresaUpdateView.as_view(), name='empresa-update'),  # Atualização de empresa
+    path('empresa/<int:pk>/delete/', EmpresaDeleteView.as_view(), name='empresa-delete'),  # Exclusão de empresa
+
+
+
 
 ]
 
