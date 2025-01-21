e779230851d2fb9dab6e7beb3aa74bc53ff8de62
Liandro Bulegon <lbulegon@gmail.com>
2025-01-21 16:53:14 -0300

    model

--- motopro/models.py
+++ motopro/models.py
@@ -26,16 +26,16 @@ class cidade(models.Model):
     def __str__(self):
         return self.nome
 
-
+from django.db import models
 
 class bairro(models.Model):
-    id     = models.IntegerField()
-    nome   = models.CharField(max_length=255)
-    cidade = models.ForeignKey (cidade, on_delete=models.CASCADE)
+    id      = models.AutoField(primary_key=True)  # Continua sendo chave primária única
+    nome    = models.CharField(max_length=255)
+    cidade  = models.ForeignKey( cidade , on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['id', 'cidade'], name='bairro_primary_key')
+            models.UniqueConstraint(fields=['id', 'cidade'], name='unique_bairro_per_city'),
         ]
 
     def __str__(self):
