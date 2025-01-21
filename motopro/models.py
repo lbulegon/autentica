7279c3417e4fcf8fa27a472bf206f73dd22ec462
Liandro Bulegon <lbulegon@gmail.com>
2025-01-21 16:18:03 -0300

    nome do estado

--- motopro/models.py
+++ motopro/models.py
@@ -20,7 +20,7 @@ class estado(models.Model):
 class cidade(models.Model):
     id           = models.AutoField(primary_key=True)
     nome         = models.CharField(max_length=255)
-    todos_estado = models.ForeignKey(estado, on_delete=models.CASCADE)
+    estado_is    = models.ForeignKey(estado, on_delete=models.CASCADE)
     codigo_ibge  = models.CharField(max_length=10, unique=True, null=True, blank=True)
 
     def __str__(self):
@@ -155,8 +155,6 @@ class Candidatura(models.Model):
 
     def __str__(self):
         return f"{self.motoboy.nome} - {self.vaga.titulo} ({self.status})"
-
-
 
 
 class avaliacao(models.Model):
