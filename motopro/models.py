1e434842d7ca254fa0531271c7008e0860d4772e
Liandro Bulegon <lbulegon@gmail.com>
2025-01-24 13:14:50 -0300

    add

--- motopro/models.py
+++ motopro/models.py
@@ -1,6 +1,6 @@
 
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator, MaxValueValidator
+#from django.core.exceptions import ValidationError
+#from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 import datetime
