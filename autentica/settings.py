3855822ef5de71f2c3da105d619a0d6993161693
lbulegon <lbulegon@gmail.com>
2025-02-11 22:54:34 -0300

    add

--- autentica/settings.py
+++ autentica/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'autentica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],  # Diretório global de templates
+        'DIRS': [BASE_DIR/"templates"],  # Diretório global de templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
