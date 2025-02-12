fca73ab5c6f31cf2da49a624f2eb9a6ca23aac55
lbulegon <lbulegon@gmail.com>
2025-02-11 22:38:42 -0300

    add

--- motopro/models.py
+++ motopro/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
-
-
 import datetime
 
 
