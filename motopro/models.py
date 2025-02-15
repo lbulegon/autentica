16f594df8872e100d009e8a6238ee11e92ca2c51
lbulegon <lbulegon@gmail.com>
2025-02-15 20:03:42 -0300

    add

--- motopro/models.py
+++ motopro/models.py
@@ -39,7 +39,6 @@ class bairro(models.Model):
     def __str__(self):
         return self.nome
  
-
 class motoboy(models.Model):
     id                 = models.AutoField(primary_key=True)
     nome               = models.CharField(max_length=255, null=False, blank=False)
