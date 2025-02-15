926c72ba89ae82074af8d564bc016cc9f2fca700
lbulegon <lbulegon@gmail.com>
2025-02-15 19:24:42 -0300

    add

--- motopro/models.py
+++ motopro/models.py
@@ -43,19 +43,7 @@ class supervisor(models.Model):
     id                 = models.AutoField(primary_key=True)
     nome               = models.CharField(max_length=255, null=False, blank=False)
     cep                = models.CharField(max_length=10)
-    estado             = models.ForeignKey(estado, on_delete=models.PROTECT)
-    cidade             = models.ForeignKey(cidade, on_delete=models.PROTECT)
-    bairro             = models.ForeignKey(bairro, on_delete=models.PROTECT)
-    logradouro         = models.CharField(max_length=255)
-    numero             = models.CharField(max_length=10)
-    complemento        = models.CharField(max_length=100, blank=True)
-    created_at         = models.DateTimeField(auto_now_add= True, null=False, blank=False)
-    finished_at        = models.DateTimeField(null=True) 
-  
-    status             = models.CharField(max_length=20, choices=[
-                        ('ativo', 'Ativo'),
-                        ('inativo', 'Inativo'),
-                        ], default='ativo')  # Status do motoboy
+ 
     def __str__(self):
         return self.nome
 
