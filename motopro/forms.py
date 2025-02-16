e6937e740c28584e63812061dac20111c4d37937
lbulegon <lbulegon@gmail.com>
2025-02-16 00:35:04 -0300

    add

--- motopro/forms.py
+++ motopro/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from motopro.models import vaga
-
 class VagaForm(forms.ModelForm):
     class Meta:
         model = vaga
-        fields = ['empresa_id', 'observacoes', 'valor', 'status', 'motoboy_id']
+        fields = ['estabelecimento', 'observacoes', 'valor', 'status', 'motoboy']
