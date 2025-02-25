6e236e40b772a4c0557989e9452bbde3a30fa5f0
lbulegon <lbulegon@gmail.com>
2025-02-24 22:40:47 -0300

    add

--- autentica/urls.py
+++ autentica/urls.py
@@ -21,10 +21,11 @@ from django.conf import settings
 from django.views.static import serve
 
 from motopro.views import home 
-from motopro.views import VagaCreateView, VagaUpdateView, VagaDeleteView,VagaListView
+from motopro.views import VagaCreateView, VagaUpdateView, VagaDeleteView
 from motopro.views import EstabelecimentoCreateView, EstabelecimentoUpdateView, EstabelecimentoDeleteView, EstabelecimentoListView
 from motopro.views import MotoboyCreateView, MotoboyUpdateView, MotoboyDeleteView, MotoboyListView
 from motopro.views import SupervisorCreateView, SupervisorUpdateView, SupervisorDeleteView, SupervisorListView
+from motopro.views import VagaListView  # Certifique-se de que está importando a classe corretamente
 
 urlpatterns = [
     
@@ -35,42 +36,39 @@ urlpatterns = [
    
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
-   
-    ######### users   ####################
- #   path('users/',                 UserListView.as_view(),   name='user-list'),
- #    path('users/create/',          UserCreateView.as_view(), name='user-create'),
-  #   path('users/update/<int:pk>/', UserUpdateView.as_view(), name='user-update'),
-  #   path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),
-   
-   ########## vagas ################
-     path('vagas/',                 VagaListView.as_view(),   name='vaga-list'),
-   
-     path('vagas/create/',          VagaCreateView.as_view(), name='vaga-create'),
-     path('vagas/update/<int:pk>/', VagaUpdateView.as_view(), name='vaga-update'),
-     path('vagas/delete/<int:pk>/', VagaDeleteView.as_view(), name='vaga-delete'),
-
-    ########## Empesas ################
-  #   path('empresas/',                EmpresaListView.as_view(),   name='empresa-list'),  # Lista de empresas
- #    path('empresa/<int:pk>/',        EmpresaDetailView.as_view(), name='empresa-detail'),  # Detalhe de uma empresa
- #    path('empresa/create/',          EmpresaCreateView.as_view(), name='empresa-create'),  # Criação de empresa
- #    path('empresa/<int:pk>/update/', EmpresaUpdateView.as_view(), name='empresa-update'),  # Atualização de empresa
- #    path('empresa/<int:pk>/delete/', EmpresaDeleteView.as_view(), name='empresa-delete'),  # Exclusão de empresa
-
-    # Estabelecimentos
+########## vagas ################
+    path('vagas/',                 VagaListView.as_view(),   name='vaga-list'),
+    path('vagas/create/',          VagaCreateView.as_view(), name='vaga-create'),
+    path('vagas/update/<int:pk>/', VagaUpdateView.as_view(), name='vaga-update'),
+    path('vagas/delete/<int:pk>/', VagaDeleteView.as_view(), name='vaga-delete'),
+########## Estabelecimentos ################
     path('estabelecimentos/', EstabelecimentoListView.as_view(), name='estabelecimento-list'),
     path('estabelecimentos/create/', EstabelecimentoCreateView.as_view(), name='estabelecimento-create'),
     path('estabelecimentos/update/<int:pk>/', EstabelecimentoUpdateView.as_view(), name='estabelecimento-update'),
     path('estabelecimentos/delete/<int:pk>/', EstabelecimentoDeleteView.as_view(), name='estabelecimento-delete'),
-    
+########## Motoboy ################   
     path('motoboy', MotoboyListView.as_view(), name='motoboy-list'),
     path('motoboy/create/', MotoboyCreateView.as_view(), name='motoboy-create'),
     path('motoboy/update/<int:pk>/', MotoboyUpdateView.as_view(), name='motoboy-update'),
     path('motoboy/delete/<int:pk>/', MotoboyDeleteView.as_view(), name='motoboy-delete'),
-
+########## Supervisor ################
     path('supervisor/', SupervisorListView.as_view(), name='supervisor-list'),
     path('supervisor/create/', SupervisorCreateView.as_view(), name='supervisor-create'),
     path('supervisor/update/<int:pk>/', SupervisorUpdateView.as_view(), name='supervisor-update'),
     path('supervisor/delete/<int:pk>/', SupervisorDeleteView.as_view(), name='supervisor-delete'),
 
+    ########## Empesas ################
+ #    path('empresas/',                EmpresaListView.as_view(),   name='empresa-list'),  # Lista de empresas
+ #    path('empresa/<int:pk>/',        EmpresaDetailView.as_view(), name='empresa-detail'),  # Detalhe de uma empresa
+ #    path('empresa/create/',          EmpresaCreateView.as_view(), name='empresa-create'),  # Criação de empresa
+ #    path('empresa/<int:pk>/update/', EmpresaUpdateView.as_view(), name='empresa-update'),  # Atualização de empresa
+ #    path('empresa/<int:pk>/delete/', EmpresaDeleteView.as_view(), name='empresa-delete'),  # Exclusão de empresa
+  
+    ######### users   ####################
+ #    path('users/',                 UserListView.as_view(),   name='user-list'),
+ #    path('users/create/',          UserCreateView.as_view(), name='user-create'),
+ #    path('users/update/<int:pk>/', UserUpdateView.as_view(), name='user-update'),
+ #    path('users/delete/<int:pk>/', UserDeleteView.as_view(), name='user-delete'),
+   
 ]
 
