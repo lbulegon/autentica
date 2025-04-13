e7d0d7cdde363ce1a671ec3773f742aae404c7fe
lbulegon <lbulegon@gmail.com>
2025-04-12 21:59:03 -0300

    add

--- autentica/celery.py
+++ autentica/celery.py
@@ -1,9 +1,13 @@
 # myproject/celery.py
+"""
 import os
+
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-app = Celery('myproject')
+app = Celery('motopro')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+"""
