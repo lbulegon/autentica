015c5cfc8f9b43754eb45d2ded4ca0a6b291647e
Liandro  Bulegon <lbulegon@gmail.com>
2025-04-21 02:52:38 -0300

    adm

--- motopro/admin.py
+++ motopro/admin.py
@@ -9,23 +9,6 @@ admin.site.register(motoboy)
 admin.site.register(supervisor)
 admin.site.register(supervisorestabelecimento)
 admin.site.register(supervisormotoboy)
-
-
-class VagaAdmin(admin.ModelAdmin):
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "motoboy":
-            object_id = request.resolver_match.kwargs.get("object_id")
-            if object_id:
-                vaga = vaga.objects.get(pk=object_id)
-                estabelecimento = vaga.contrato.estabelecimento
-
-                supervisor_rel = supervisorestabelecimento.objects.filter(estabelecimento=estabelecimento).first()
-                if supervisor_rel:
-                    supervisor = supervisor_rel.supervisor
-                    motoboy_ids = supervisormotoboy.objects.filter(supervisor=supervisor).values_list('motoboy_id', flat=True)
-                    kwargs["queryset"] = motoboy.objects.filter(id__in=motoboy_ids)
-
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-admin.site.register(vaga, VagaAdmin)
+admin.site.register(estabelecimentocontrato)
+admin.site.register(vaga)
 
